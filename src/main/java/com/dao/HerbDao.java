ceeab360da5a147ba568ebe12d4de4b05f117220
firsticz <thanawat6368@gmail.com>
2018-10-03 17:31:27 +0700

    edit herb dao ato

--- src/main/java/com/dao/HerbDao.java
+++ src/main/java/com/dao/HerbDao.java
@@ -1,62 +1,26 @@
 package com.dao;
 
 public class HerbDao {
-	int _id;
-	String name;
-	String herbtype;
-	String grouptype;
-	String formula;
-	String help;
-	String[] solution;
-	String warning;
-	public int get_id() {
-		return _id;
+	String herbname;
+	String[] properties;
+	String[] warning;
+	public String getHerbname() {
+		return herbname;
 	}
-	public void set_id(int _id) {
-		this._id = _id;
+	public void setHerbname(String herbname) {
+		this.herbname = herbname;
 	}
-	public String getName() {
-		return name;
+	public String[] getProperties() {
+		return properties;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setProperties(String[] properties) {
+		this.properties = properties;
 	}
-	public String getHerbtype() {
-		return herbtype;
-	}
-	public void setHerbtype(String herbtype) {
-		this.herbtype = herbtype;
-	}
-	public String getGrouptype() {
-		return grouptype;
-	}
-	public void setGrouptype(String grouptype) {
-		this.grouptype = grouptype;
-	}
-	public String getFormula() {
-		return formula;
-	}
-	public void setFormular(String formula) {
-		this.formula = formula;
-	}
-	public String getHelp() {
-		return help;
-	}
-	public void setHelp(String help) {
-		this.help = help;
-	}
-	public String[] getSolution() {
-		return solution;
-	}
-	public void setSolution(String[] solution) {
-		this.solution = solution;
-	}
-	public String getWarning() {
+	public String[] getWarning() {
 		return warning;
 	}
-	public void setWarning(String warning) {
+	public void setWarning(String[] warning) {
 		this.warning = warning;
 	}
-	
 	
 }
