c947d28078cf5e31cf8eb6cd852f48bd2e2ada22
tdedmalaew <nuttawut.c39@gmail.com>
2018-10-03 08:29:31 +0700

    login

--- src/main/java/com/controller/Register.java
+++ src/main/java/com/controller/Register.java
@@ -10,6 +10,7 @@ import org.bson.Document;
 import org.modelmapper.ModelMapper;
 
 import com.connect.mongo.Connect;
+import com.dao.Login;
 import com.dao.RegisterDao;
 import com.dto.RegisterDto;
 import com.google.gson.Gson;
@@ -32,6 +33,30 @@ public class Register {
 		RegisterDao registerDao = Mapper.map(registerDto, RegisterDao.class);
 		
 		String json = gson.toJson(registerDao);
+		Document document = Document.parse(json);
+		
+		try {
+			collection.insertOne(document);
+			message.addProperty("message", true);
+		}catch (Exception e) {
+			message.addProperty("message", false);
+		}
+		
+		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+	}
+	
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response login(RegisterDto loginDto) {
+		Connect mongo = new Connect();
+		JsonObject message = new JsonObject();
+		Gson gson = new Gson();
+		MongoCollection<Document> collection = mongo.db.getCollection("member");
+		ModelMapper Mapper = new ModelMapper();
+		Login loginDao = Mapper.map(loginDto, Login.class);
+		
+		String json = gson.toJson(loginDao);
 		Document document = Document.parse(json);
 		
 		try {
