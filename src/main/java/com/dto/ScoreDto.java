ff8be9f529c8d92ca7aa23553d03d6d1e0a7c062
firsticz <thanawat6368@gmail.com>
2018-10-03 17:54:27 +0700

    add score dto dao

---
+++ src/main/java/com/dto/ScoreDto.java
@@ -0,0 +1,26 @@
+package com.dto;
+
+public class ScoreDto {
+	String drugformula;
+	double score;
+	int vote;
+	public String getDrugformula() {
+		return drugformula;
+	}
+	public void setDrugformula(String drugformula) {
+		this.drugformula = drugformula;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public int getVote() {
+		return vote;
+	}
+	public void setVote(int vote) {
+		this.vote = vote;
+	}
+	
+}
