14b1683aa5d2927a9a1970eb9ae5e2d446b44c81
firsticz <thanawat6368@gmail.com>
2018-10-03 17:37:47 +0700

    add symptomgroup dto dao

---
+++ src/main/java/com/dao/symptomgroupDao.java
@@ -0,0 +1,15 @@
+package com.dao;
+
+public class symptomgroupDao {
+
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/main/java/com/dto/symptomgroupDto.java
@@ -0,0 +1,15 @@
+package com.dto;
+
+public class symptomgroupDto {
+
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
