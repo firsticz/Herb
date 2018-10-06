3ebd4bb199bd97076791ddf885ea2d8249bd5cba
tdedmalaew <nuttawut.c38@gmail.com>
2018-10-06 17:07:52 +0700

    commit SymtomGroup&Sytom

--- src/main/java/com/controller/Symtoms.java
+++ src/main/java/com/controller/Symtoms.java
@@ -1,5 +1,6 @@
 package com.controller;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
@@ -7,10 +8,15 @@ import org.bson.Document;
 import org.modelmapper.ModelMapper;
 import com.connect.Connect;
 import com.dao.SymtomDao;
+import com.dao.UpdateDao;
+import com.dto.RegisterDto;
 import com.dto.SymtomDto;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+
 import javax.ws.rs.core.Response;
 @Path("/systoms")
 public class Symtoms {
@@ -33,6 +39,55 @@ public class Symtoms {
 			
 			try {
 				collection.insertOne(document);
+				message.addProperty("message", true);
+			}catch (Exception e) {
+				message.addProperty("message", false);
+			}
+			
+			return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+		}
+		
+		@POST
+		@Path("/update")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response update(SymtomDto updateDto) {
+			Connect mongo = new Connect();
+			JsonObject message = new JsonObject();
+			Gson gson = new Gson();
+			MongoCollection<Document> collection = mongo.db.getCollection("systoms");
+			ModelMapper Mapper = new ModelMapper();
+			SymtomDao updateDao = Mapper.map(updateDto, SymtomDao.class);
+			
+			String json = gson.toJson(updateDao);
+			Document document = Document.parse(json);
+			
+			BasicDBObject setQuery = new BasicDBObject();
+	        setQuery.put("$set", document);
+			
+			BasicDBObject searchQuery = new BasicDBObject();
+			searchQuery.put("_id", updateDto.getId());
+			
+			try {
+				collection.updateOne(searchQuery, setQuery);
+				message.addProperty("message", true);
+			}catch (Exception e) {
+				message.addProperty("message", false);
+			}
+			
+			return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+		}
+		
+		@DELETE
+		@Path("/delete")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response delete(SymtomDto deleteDto) {
+			Connect mongo = new Connect();
+			JsonObject message = new JsonObject();
+			Gson gson = new Gson();
+			MongoCollection<Document> collection = mongo.db.getCollection("systoms");
+			
+			try {
+				collection.deleteOne(Filters.eq("_id", deleteDto.getId())); 
 				message.addProperty("message", true);
 			}catch (Exception e) {
 				message.addProperty("message", false);
