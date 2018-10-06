81753730fedb0fe32950c2d491f0c629a8997e10
tdedmalaew <nuttawut.c38@gmail.com>
2018-10-06 16:58:51 +0700

    commit herb update delete

--- src/main/java/com/controller/Herb.java
+++ src/main/java/com/controller/Herb.java
@@ -1,5 +1,6 @@
 package com.controller;
  import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
@@ -8,10 +9,14 @@ import javax.ws.rs.core.Response;
 import org.modelmapper.ModelMapper;
  import com.connect.Connect;
 import com.dao.HerbDao;
+import com.dao.UpdateDao;
 import com.dto.HerbDto;
+import com.dto.RegisterDto;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
  @Path("/herb")
 public class Herb {
 	
@@ -34,6 +39,55 @@ public class Herb {
 		
 		try {
 			collection.insertOne(document);
+			message.addProperty("message", true);
+		}catch (Exception e) {
+			message.addProperty("message", false);
+		}
+		
+		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+	}
+	
+	@POST
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(HerbDto updateDto) {
+		Connect mongo = new Connect();
+		JsonObject message = new JsonObject();
+		Gson gson = new Gson();
+		MongoCollection<Document> collection = mongo.db.getCollection("herb");
+		ModelMapper Mapper = new ModelMapper();
+		HerbDao updateDao = Mapper.map(updateDto, HerbDao.class);
+		
+		String json = gson.toJson(updateDao);
+		Document document = Document.parse(json);
+		
+		BasicDBObject setQuery = new BasicDBObject();
+        setQuery.put("$set", document);
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("_id", updateDto.getId());
+		
+		try {
+			collection.updateOne(searchQuery, setQuery);
+			message.addProperty("message", true);
+		}catch (Exception e) {
+			message.addProperty("message", false);
+		}
+		
+		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+	}
+	
+	@DELETE
+	@Path("/delete")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response delete(HerbDto deleteDto) {
+		Connect mongo = new Connect();
+		JsonObject message = new JsonObject();
+		Gson gson = new Gson();
+		MongoCollection<Document> collection = mongo.db.getCollection("herb");
+		
+		try {
+			collection.deleteOne(Filters.eq("_id", deleteDto.getId())); 
 			message.addProperty("message", true);
 		}catch (Exception e) {
 			message.addProperty("message", false);
