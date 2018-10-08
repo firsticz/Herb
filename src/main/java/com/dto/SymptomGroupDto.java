a2eb78b0daee0254b491f5a2af5600b30a8e36cc
firsticz <thanawat6368@gmail.com>
2018-10-08 13:18:11 +0700

    edit symptomgroup

--- src/main/java/com/dto/SymptomGroupDto.java
+++ src/main/java/com/dto/SymptomGroupDto.java
@@ -1,22 +1,24 @@
 package com.dto;
 
+import org.bson.types.ObjectId;
+
 public class SymptomGroupDto {
 	
-	static String id;
-	static String SymtomGroupName;
-	
-	public static String getId() {
-		return id;
+	public String id;
+	public String SymtomGroupName;
+	public ObjectId getId() {
+		return new ObjectId(id);
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public static String getSymtomGroupName() {
+	public String getSymtomGroupName() {
 		return SymtomGroupName;
 	}
 	public void setSymtomGroupName(String symtomGroupName) {
 		SymtomGroupName = symtomGroupName;
 	}
+	
 	
 	
 }
