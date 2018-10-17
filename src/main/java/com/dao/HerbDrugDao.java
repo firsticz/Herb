5725590015f3f21ce2a4416182dc56ec1a425514
firsticz <thanawat6368@gmail.com>
2018-10-17 18:13:20 +0700

    edit herbdrug

--- src/main/java/com/dao/HerbDrugDao.java
+++ src/main/java/com/dao/HerbDrugDao.java
@@ -2,20 +2,21 @@ package com.dao;
 
 public class HerbDrugDao {
 	
-	HerbDao idherb;
+	String idherb;
 	String weight;
 	
-	public HerbDao getIdherb() {
-		return idherb;
-	}
-	public void setIdherb(HerbDao idherb) {
-		this.idherb = idherb;
-	}
 	public String getWeight() {
 		return weight;
 	}
 	public void setWeight(String weight) {
 		this.weight = weight;
 	}
+	public String getIdherb() {
+		return idherb;
+	}
+	public void setIdherb(String idherb) {
+		this.idherb = idherb;
+	}
+	
 	
 }
--- src/main/java/com/dto/HerbDrugDto.java
+++ src/main/java/com/dto/HerbDrugDto.java
@@ -2,20 +2,22 @@ package com.dto;
 
 public class HerbDrugDto {
 	
-	HerbDto idherb;
+	String idherb;
 	String weight;
 	
-	public HerbDto getIdherb() {
-		return idherb;
-	}
-	public void setIdherb(HerbDto idherb) {
-		this.idherb = idherb;
-	}
+	
 	public String getWeight() {
 		return weight;
 	}
 	public void setWeight(String weight) {
 		this.weight = weight;
 	}
+	public String getIdherb() {
+		return idherb;
+	}
+	public void setIdherb(String idherb) {
+		this.idherb = idherb;
+	}
+	
 	
 }
