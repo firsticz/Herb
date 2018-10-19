930adf765b38c5c770563fb89b6794a7f239491c
firsticz <thanawat6368@gmail.com>
2018-10-19 10:40:39 +0700

    add socre output

---
+++ src/main/java/com/dto/ScoreOutput.java
@@ -0,0 +1,14 @@
+package com.dto;
+
+public class ScoreOutput {
+	public double[] avg;
+
+	public double[] getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double[] avg) {
+		this.avg = avg;
+	}
+	
+}
